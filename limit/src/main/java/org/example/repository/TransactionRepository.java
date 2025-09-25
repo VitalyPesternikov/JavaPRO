1ba1cfe715af2e93d1b89e27ee3e3a9fe9b78755
vitaliy_pesternikov <pesternikov94@gmail.com>
2025-09-25 15:57:28 +0200

    Исправление замечаний, доработка сервиса, добавление возможности откатить платёж

--- limit/src/main/java/org/example/repository/TransactionRepository.java
+++ limit/src/main/java/org/example/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package org.example.repository;
 
 import org.example.model.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- limit/src/main/java/org/example/service/LimitService.java
+++ limit/src/main/java/org/example/service/LimitService.java
@@ -1,7 +1,6 @@
 package org.example.service;
 
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dto.LimitDto;
